73a9812a53b8d943453541588d1fa9ee227018de
Dennis Schumann <dennis.schumann@devbliss.com>
2014-10-07 12:00:26 +0200

    Added license doc.

--- src/main/groovy/com/devbliss/doctest/DoctestBasePlugin.groovy
+++ src/main/groovy/com/devbliss/doctest/DoctestBasePlugin.groovy
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2014, devbliss GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.devbliss.doctest
 
 import org.gradle.api.Plugin
@@ -5,18 +19,20 @@ import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.api.plugins.JavaBasePlugin
 
 /**
+ * BasePlugin for doctest plugin.
+ * Applies {@link JavaBasePlugin} and adds it to the source set defaults.
  *
  * @author Dennis Schumann <dennis.schumann@devbliss.com>
  */
 class DoctestBasePlugin implements Plugin<ProjectInternal> {
-  
+
   private ProjectInternal project;
-	
+
   public void apply(ProjectInternal project) {
     this.project = project
-    
+
     JavaBasePlugin javaBasePlugin = project.getPlugins().apply(JavaBasePlugin.class)
-    
+
     configureSourceSetDefaults(javaBasePlugin)
   }
 }
--- src/main/groovy/com/devbliss/doctest/task/DoctestTask.groovy
+++ src/main/groovy/com/devbliss/doctest/task/DoctestTask.groovy
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2014, devbliss GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.devbliss.doctest.task
 
 import com.devbliss.doctest.Configuration
@@ -25,11 +39,11 @@ import org.gradle.api.tasks.testing.Test
 class DoctestTask extends Test {
   public static final String DOCTEST_RESULT_DIR_NAME = "doctests-result";
   public static final String DOCTEST_REPORT_DIR_NAME = "doctests";
-  
+
   File reportDir;
-  
+
   String docHtmlDir;
-  
+
   public DoctestTask() {
     super();
     reportDir = new File(getProject().getBuildDir(), "reports/" + DOCTEST_REPORT_DIR_NAME);
@@ -39,12 +53,12 @@ class DoctestTask extends Test {
     File doctestBinResultDir = new File(getProject().getBuildDir(), DOCTEST_RESULT_DIR_NAME + "/binary/" + DOCTEST_REPORT_DIR_NAME);
     setBinResultsDir(doctestBinResultDir);
   }
-  
+
   public void setReportDir(File reportDir) {
     this.reportDir = reportDir;
     getReports().getHtml().setDestination(this.reportDir);
   }
-  
+
   public void setDocHtmlDir(String docHtmlDir) {
     this.docHtmlDir = docHtmlDir;
     this.systemProperty(Configuration.PROPERTY_HTML_OUTPUT_DIR, this.docHtmlDir)
