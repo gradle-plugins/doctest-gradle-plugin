9cfff596880b5d130fdc8bf6dec46def7a373cbe
Dennis Schumann <hillkorn@gmail.com>
2015-07-19 19:25:04 +0200

    Renamed packages to org.gradliss.

--- src/main/groovy/org/gradliss/doctest/DoctestPlugin.groovy
+++ src/main/groovy/org/gradliss/doctest/DoctestPlugin.groovy
@@ -1,20 +1,20 @@
 /*
  * Copyright 2014, devbliss GmbH
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 
-package com.devbliss.doctest
+package org.gradliss.doctest
 
-import com.devbliss.doctest.task.DoctestTask
+import org.gradliss.doctest.task.DoctestTask
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -36,29 +36,29 @@ import java.util.concurrent.Callable
  */
 class DoctestPlugin implements Plugin<Project> {
   public static final String DOCTEST_SOURCE_SET_NAME = "doctest";
-  
+
   public static final String DOCTEST_TASK_NAME = "doctest";
-  
+
   public static final String DOCTEST_RUNTIME_CONFIGURATION_NAME = "doctestRuntime";
   public static final String DOCTEST_COMPILE_CONFIGURATION_NAME = "doctestCompile";
-  
+
   def void apply(Project project) {
     project.getPlugins().apply(JavaPlugin.class);
-    
+
     JavaBasePlugin javaBasePlugin = project.getPlugins().apply(JavaBasePlugin.class);
     JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
-    
+
     configureSourceSets(javaBasePlugin, project);
     configureConfigurations(project);
-    
+
     configureTest(javaConvention, project);
   }
-  
+
   private void configureSourceSets(final JavaBasePlugin javaBasePlugin, Project project) {
     SourceSetContainer sourceSetContainer = project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets();
     SourceSet main = sourceSetContainer.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
     SourceSet doctest = sourceSetContainer.create(DOCTEST_SOURCE_SET_NAME);
-    
+
     doctest.setCompileClasspath(project.files(main.getOutput(), project.getConfigurations().getByName(DOCTEST_COMPILE_CONFIGURATION_NAME)));
     doctest.setRuntimeClasspath(project.files(doctest.getOutput(), main.getOutput(), project.getConfigurations().getByName(DOCTEST_RUNTIME_CONFIGURATION_NAME)));
   }
@@ -75,7 +75,7 @@ class DoctestPlugin implements Plugin<Project> {
 
     configurations.getByName(Dependency.DEFAULT_CONFIGURATION).extendsFrom(runtimeConfiguration);
   }
-  
+
   private void configureTest(JavaPluginConvention javaConvention, Project project) {
     project.getTasks().withType(DoctestTask.class, new Action<DoctestTask>() {
         public void execute(final DoctestTask doctest) {
